33697d39405507515d344bc37238f12e5163794b
brasid <alexanderfuf@gmail.com>
2019-01-10 13:42:11 +0300

    update fixture

--- __tests__/__fixtures__/assets/script.js
+++ __tests__/__fixtures__/assets/script.js
@@ -1,5 +1,12 @@
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WK88TH');
+import path from 'path';
+import url from 'url';
+import _ from 'lodash';
+
+export default (address) => {
+  const { hostname, pathname } = url.parse(address);
+  const filename = hostname ? path.join(hostname, pathname) : _.trim(pathname, '/');
+  const { dir, name, ext } = path.parse(filename);
+  const newFilename = path.join(dir, name).replace(/\W+/g, '-');
+  const newPathName = path.format({ name: newFilename, ext });
+  return newPathName;
+};
