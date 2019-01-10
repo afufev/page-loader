41b1b511a3ad04edf115e6bd4e54d6159a3c6336
brasid <alexanderfuf@gmail.com>
2019-01-10 13:46:31 +0300

    fix linter

--- __tests__/__fixtures__/assets/script.js
+++ __tests__/__fixtures__/assets/script.js
@@ -1,12 +1,3 @@
-import path from 'path';
 import url from 'url';
-import _ from 'lodash';
 
-export default (address) => {
-  const { hostname, pathname } = url.parse(address);
-  const filename = hostname ? path.join(hostname, pathname) : _.trim(pathname, '/');
-  const { dir, name, ext } = path.parse(filename);
-  const newFilename = path.join(dir, name).replace(/\W+/g, '-');
-  const newPathName = path.format({ name: newFilename, ext });
-  return newPathName;
-};
+export default address => url.parse(address);
